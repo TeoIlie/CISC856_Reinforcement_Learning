d2768d60ccb9732c896d9bd216cb2bdf40c8528d
TeoIlie <teo.altum.quinque@gmail.com>
2025-03-05 16:11:40 -0500

    added convergence q

--- control_algorithms.py
+++ control_algorithms.py
@@ -7,7 +7,7 @@ GAMMA = 1.0
 
 
 def sarsa(env, episodes, alpha, gamma, epsilon):
-    """Use Sarsa control algorithm for given parameter values."""
+    """Use Sarsa control algorithm for given number of episodes."""
     # Q hold all values for (row, col, action) triplets
     Q = np.zeros((env.rows, env.cols, env.num_actions))
 
@@ -63,7 +63,7 @@ def sarsa(env, episodes, alpha, gamma, epsilon):
 def sarsa_to_convergence(
     env, optimal_path_length, alpha=ALPHA, epsilon=EPSILON, gamma=GAMMA
 ):
-    """Use Sarsa control algorithm for given parameter values."""
+    """Sarsa control algorithm until optimal path reached."""
     # Q hold all values for (row, col, action) triplets
     Q = np.zeros((env.rows, env.cols, env.num_actions))
 
@@ -74,7 +74,7 @@ def sarsa_to_convergence(
     steps = []
     episode = 0
 
-    # Loop for each episode
+    # Loop to convergence
     while env.get_optimal_path_size(Q) != optimal_path_length:
         # Initialize state and other params
         state = env.reset()
@@ -119,7 +119,7 @@ def sarsa_to_convergence(
 
 
 def q_learning(env, episodes, alpha, gamma, epsilon):
-    """Q-learning off-policy control"""
+    """Q-learning off-policy control for given number of episodes"""
     # Q holds all values for (row, col, action) triplets
     Q = np.zeros((env.rows, env.cols, env.num_actions))
 
@@ -152,10 +152,10 @@ def q_learning(env, episodes, alpha, gamma, epsilon):
             episode_numbers.append(episode)
 
             # Q-learning update rule
-            greedy_path_size = np.argmax(Q[next_state[0], next_state[1], :])
+            greedy_next_action = np.argmax(Q[next_state[0], next_state[1], :])
             Q[state[0], state[1], action] += alpha * (
                 reward
-                + gamma * Q[next_state[0], next_state[1], greedy_path_size]
+                + gamma * Q[next_state[0], next_state[1], greedy_next_action]
                 - Q[state[0], state[1], action]
             )
 
@@ -163,5 +163,58 @@ def q_learning(env, episodes, alpha, gamma, epsilon):
             state = next_state
 
         steps[episode] = step_count
+
+    return Q, steps, time_steps, episode_numbers
+
+
+def q_learning_to_convergence(
+    env, optimal_path_length, alpha=ALPHA, epsilon=EPSILON, gamma=GAMMA
+):
+    """Q-learning algorithm until convergence reached"""
+    # Q holds all values for (row, col, action) triplets
+    Q = np.zeros((env.rows, env.cols, env.num_actions))
+
+    # Keep track of stats for graphing
+    time_steps = []
+    episode_numbers = []
+    total_steps = 0
+    steps = []
+    episode = 0
+
+    # Loop to convergence
+    while env.get_optimal_path_size(Q) != optimal_path_length:
+        # Initialize state and other params
+        state = env.reset()
+        total_reward = 0
+        step_count = 0
+        done = False
+
+        # Continue until terminal (goal) state reached
+        while not done:
+            # Choose action using epsilon-greedy selection
+            action = env.epsilon_greedy_policy(Q, state, epsilon, env.num_actions)
+
+            # Take an action, observe reward and new state
+            next_state, reward, done = env.step(action)
+            total_reward += reward
+            step_count += 1
+            total_steps += 1
+
+            time_steps.append(total_steps)
+            episode_numbers.append(episode)
+
+            # Q-learning update rule
+            greedy_next_action = np.argmax(Q[next_state[0], next_state[1], :])
+            Q[state[0], state[1], action] += alpha * (
+                reward
+                + gamma * Q[next_state[0], next_state[1], greedy_next_action]
+                - Q[state[0], state[1], action]
+            )
+
+            # Move to next state
+            state = next_state
+
+        steps.append(step_count)
+        episode += 1
 
     return Q, steps, time_steps, episode_numbers
--- convergence.py
+++ convergence.py
@@ -1,4 +1,3 @@
-import numpy as np
 import windygridworld
 import visualize
 import click
@@ -9,14 +8,29 @@ OPTIMAL_PATH_LENGTH = 15
 
 if __name__ == "__main__":
     env = windygridworld.WindyGridworld()
-    runs = 20
+    runs = 50
     total_time_steps = 0
     total_episodes = 0
 
+    print("\nSelect one of Sarsa[s], Q-Learning[q], Q(λ) [ql], and Sarsa(λ) [sl]:")
+    options = {"s": "Sarsa", "q": "Q-Learning", "sl": "Sarsa(λ)", "ql": "Q(λ)"}
+    control_type = click.prompt("Choose one", type=click.Choice(options.keys()))
+
     for i in range(1, runs + 1):
-        Q, _, time_steps, episode_numbers = control_algorithms.sarsa_to_convergence(
-            env, OPTIMAL_PATH_LENGTH
-        )
+        if control_type == "s":
+            Q, _, time_steps, episode_numbers = control_algorithms.sarsa_to_convergence(
+                env, OPTIMAL_PATH_LENGTH
+            )
+        elif control_type == "q":
+            Q, _, time_steps, episode_numbers = (
+                control_algorithms.q_learning_to_convergence(env, OPTIMAL_PATH_LENGTH)
+            )
+        elif control_type == "sl":
+            # TODO
+            pass
+        elif control_type == "ql":
+            # TODO
+            pass
 
         curr_total_steps = time_steps[-1]
         total_time_steps += curr_total_steps
@@ -29,13 +43,13 @@ if __name__ == "__main__":
         )
 
     print(
-        f"Average convergence metrics over {runs} runs: \
+        f"{options[control_type]} average convergence metrics over {runs} runs: \
         \nAverage time steps: {total_time_steps/runs} \
         \nAverage episodes: {total_episodes/runs}\n"
     )
 
     # Visualize the learned policy
-    print(f"Sarsa Optimal Policy")
+    print("Policy")
     visualize.visualize_policy(env, Q)
 
     # Plot episode vs time steps
