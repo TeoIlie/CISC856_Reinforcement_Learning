41d8a52d781725fbb6de4e3bf4f29b9898dc2834
TeoIlie <teo.altum.quinque@gmail.com>
2025-03-11 12:57:45 -0400

    added train/load option

--- Gym/PPO_cartpole.py
+++ Gym/PPO_cartpole.py
@@ -1,24 +1,34 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+import os
+
+MODEL_DIR = "models"
+PPO_MODEL_DIR = f"{MODEL_DIR}/ppo_cartpole"
+TRAIN = False
 
 
 def train(env):
-    """Train a model on environment, with PPO"""
+    """Train a model on environment, with PPO, and save"""
     # Initialize the PPO model
+    print("Training model with PPO...")
     model = PPO("MlpPolicy", env, verbose=1)
 
     # Train the agent
     model.learn(total_timesteps=100000)
 
     # Save the model
-    model.save("ppo_cartpole")
+    os.makedirs(MODEL_DIR, exist_ok=True)
+    model_path = PPO_MODEL_DIR
+    model.save(model_path)
+    print(f"Model saved in {model_path}.zip")
 
     return model
 
 
 def test(env, model):
-    # Test the trained agent
+    """Test the trained agent"""
+    print("Testing model with PyGame...")
     env = gym.make("CartPole-v1", render_mode="human")
     obs, _ = env.reset()
     for _ in range(1000):
@@ -33,8 +43,14 @@ if __name__ == "__main__":
     # Create the environment (CartPole is a classic RL benchmark)
     env = make_vec_env("CartPole-v1", n_envs=4)  # Vectorized env for parallel training
 
-    # train the model
-    model = train(env)
+    # train or load the model
+    if TRAIN:
+        model = train(env)
+    else:
+        try:
+            model = PPO.load(PPO_MODEL_DIR)
+        except Exception as e:
+            print(f"Error loading model from {PPO_MODEL_DIR}")
 
     # test the model
     test(env, model)
